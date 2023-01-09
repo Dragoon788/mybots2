8c70cb08ae96e4caa56c5a5dbc71eef8f04af0ec
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2023-01-09 16:59:45 -0600

    added gravity and game runs at 300fps

--- simulate.py
+++ simulate.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI, options="--opengl2")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-for x in range(0,1000):
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+
+p.loadSDF("box.sdf")
+
+for x in range(0,10000):
 	p.stepSimulation()
-	time.sleep(1/60)
-	print(x)
+	time.sleep(1/300)
 
 p.disconnect()
 
