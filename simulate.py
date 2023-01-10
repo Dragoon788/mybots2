f93619c524d7e5a8df6c8c413de4f778feb84a3a
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2023-01-09 20:37:58 -0600

    Finished Joints

--- simulate.py
+++ simulate.py
@@ -9,8 +9,10 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("body.urdf")
 
-p.loadSDF("boxes.sdf")
+
+p.loadSDF("world.sdf")
 
 for x in range(0,10000000):
 	p.stepSimulation()
