fe7afc437815dea1b209a9fadd9d67d118ca38b5
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2023-01-10 13:30:03 -0600

    touch sensors works!

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@
 import pybullet as p
 import pybullet_data
 import time
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI, options="--opengl2")
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,9 +16,13 @@ robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
 
-for x in range(0,10000000):
-	p.stepSimulation()
-	time.sleep(1/300)
-	
-p.disconnect()
+pyrosim.Prepare_To_Simulate(robotId)
 
+
+for x in range(0,10000000):
+	time.sleep(1/300)
+	p.stepSimulation()
+	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	print(backLegTouch)
+
+p.disconnect()
