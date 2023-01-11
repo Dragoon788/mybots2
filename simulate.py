b7b8e062ac4a398138589b95687c7c3c94ad72f1
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2023-01-10 22:41:12 -0600

    somewhat working robot with motors

--- simulate.py
+++ simulate.py
@@ -5,6 +5,7 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy
+import random
 
 physicsClient = p.connect(p.GUI, options="--opengl2")
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -12,7 +13,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
-t = 100
+t = 100000
 
 
 p.loadSDF("world.sdf")
@@ -23,11 +24,25 @@ backLegSensorValues = numpy.zeros(t)
 frontLegSensorValues = numpy.zeros(t)
 
 for i in range(0,t):
-	time.sleep(1/t)
+	time.sleep(1/300)
 	p.stepSimulation()
 	backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-	print(frontLegSensorValues)
+#	print(frontLegSensorValues)
+
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = b'Torso_BackLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = (random.random()*3.14),
+	maxForce = 10)
+
+	pyrosim.Set_Motor_For_Joint(
+	bodyIndex = robotId,
+	jointName = b'Torso_FrontLeg',
+	controlMode = p.POSITION_CONTROL,
+	targetPosition = (random.random()*3.14),
+	maxForce = 10)
 
 numpy.save('data/backLegSensorValues.npy', backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy', frontLegSensorValues)
