10d6c3cde79f45c2b1ffe00cf93a1fb71d7fb38e
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2023-01-21 17:16:02 -0600

    somewhat working refactoring

--- motor.py
+++ motor.py
@@ -1,3 +1,32 @@
+#!/usr/bin/python3
+
+import pybullet as p
+import pybullet_data
+import time
+import pyrosim.pyrosim as pyrosim
+import numpy
+import constants as c
+
 class MOTOR:
-	def __init__(self):
-		pass
+	def __init__(self, jointName):
+		self.jointName = jointName
+		self.Prepare_To_Act()
+
+
+	def MotorCommand(self, amplitude, frequency, offset, t):
+		targetAngles = numpy.linspace(0, 2*numpy.pi*frequency + offset,t)
+		return (numpy.sin(targetAngles))*amplitude
+
+	def Prepare_To_Act(self):
+		print(self.jointName)
+		if (self.jointName == b'Torso_BackLeg'):
+			self.motorvalues = self.MotorCommand(c.backLeg_Amp, c.backLeg_Freq/20, c.backLeg_Ofs, c.t)
+		else:
+			self.motorvalues = self.MotorCommand(c.backLeg_Amp, c.backLeg_Freq, c.backLeg_Ofs, c.t)
+	def Set_Value (self, robot, t):
+		pyrosim.Set_Motor_For_Joint(
+		bodyIndex = robot.robotId,
+		jointName = self.jointName,
+		controlMode = p.POSITION_CONTROL,
+		targetPosition = self.motorvalues[t],
+		maxForce = 50)
