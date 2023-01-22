eef7aa0d2c5e6be2f0d1df81afbbcf47cbfc4937
Dragoon788 <109482739+Dragoon788@users.noreply.github.com>
2023-01-21 19:07:44 -0600

    finished refactoring with sensor changes

--- sensor.py
+++ sensor.py
@@ -6,6 +6,7 @@ import time
 import pyrosim.pyrosim as pyrosim
 import numpy
 import constants as c
+import os
 
 class SENSOR:
 	def __init__(self, linkName):
@@ -15,3 +16,6 @@ class SENSOR:
 	def Get_Value(self,t):
 		self.values[t] = pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
 
+	def Save_Values(self):
+#		numpy.save('data/SensorValues.npy', self.values)
+		numpy.save(os.path.join('data', self.linkName + "sensor"), self.values)
