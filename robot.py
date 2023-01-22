3b7263b33977f302a27a8242b1df09df510e2d46
Dragoon788 <francisvelasco2025@u.northwestern.edu>
2023-01-21 19:03:27 -0600

    finished refactoring with sensor changes

--- robot.py
+++ robot.py
@@ -8,10 +8,10 @@ import numpy
 import constants as c
 
 from sensor import SENSOR
+from motor import MOTOR
 
 class ROBOT:
 	def __init__(self):
-		self.motors = {}
 		self.robotId = p.loadURDF("body.urdf")
 		pyrosim.Prepare_To_Simulate(self.robotId)
 		self.values = numpy.zeros(c.t)
@@ -24,3 +24,24 @@ class ROBOT:
 	def Sense(self, t):
 		for i in self.sensors:
 			self.sensors[i].Get_Value(t)
+			self.sensors[i].Save_Values()
+
+	def Prepare_To_Act(self):
+		self.motors = {}
+#		self.amplitude = c.backLeg_Amp
+#		self.frequency = c.backLeg_Freq
+#		self.offset = c.backLeg_Ofs
+
+#		self.MotorCommand = c.MotorCommand(self.amplitude, self.frequency, self.offset, c.t)
+
+		for jointName in pyrosim.jointNamesToIndices:
+			self.motors[jointName] = MOTOR(jointName)
+			self.motors[jointName].Prepare_To_Act()
+
+
+	def Act(self,t):
+		for i in self.motors:
+			self.motors[i].Set_Value(self, t)
+			self.motors[i].Save_Values()
+
+
