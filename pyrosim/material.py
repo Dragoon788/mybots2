faff923c8923fcc9f4eb84623434ef2c5f1a9a99
Dragoon788 <109482739+Dragoon788@users.noreply.github.com>
2023-02-26 20:34:20 -0600

    Add files via upload

--- pyrosim/material.py
+++ pyrosim/material.py
@@ -2,13 +2,13 @@ from pyrosim.commonFunctions import Save_Whitespace
 
 class MATERIAL: 
 
-    def __init__(self):
+    def __init__(self, color_name, color_code):
 
         self.depth  = 3
 
-        self.string1 = '<material name="Cyan">'
+        self.string1 = color_name
 
-        self.string2 = '    <color rgba="0 1.0 1.0 1.0"/>'
+        self.string2 = color_code
 
         self.string3 = '</material>'
 
